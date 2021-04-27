3ddceef46858ed1f6364e44cb3f6585fa1f6ad68
joshy manalil <manalil2000@gmail.com>
2021-04-27 10:45:16 +0530

    commit

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -10,7 +10,7 @@ public class Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("zxzczczczczczzczcz");
+		System.out.println("ANGELLAAAA");
 	}
 
 }
