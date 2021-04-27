d2796c43cc0819e99121711c85a04ddde4bd8490
joshy manalil <manalil2000@gmail.com>
2021-04-27 10:56:08 +0530

    Committing.....

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -11,6 +11,7 @@ public class Application{
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("ANGELLAAAA");
+		System.out.println("ANGELLAAAA222");
 	}
 
 }
